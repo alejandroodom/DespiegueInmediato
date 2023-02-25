7f93ded231c2ac27b91f280221cb40f039cdd69b
alejandroodom <113924039+alejandroodom@users.noreply.github.com>
2023-02-25 22:49:55 +0100

    Add files via upload

---
+++ DespliegueInmediato/DespliegueInmediato/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] tabla = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        for (int i = 0; i < tabla.length; i++) {
+            System.out.println(tabla[i]);
+        }
+    }
+}
